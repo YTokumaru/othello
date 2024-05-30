41e5f90763f78c267137de80460ca6df0e92d333
ytokumaru <ytokumaru16@gmail.com>
2024-05-31 00:47:56 +0900

    add simple tests

---
+++ test/board_test.cpp
@@ -0,0 +1,22 @@
+#include "othello/board.hpp"
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE("Board is initialized", "[Board]")
+{
+  othello::Board test_board{};
+  REQUIRE(test_board.at(3, 3) == othello::Black);
+  REQUIRE(test_board.at(3, 4) == othello::White);
+  REQUIRE(test_board.at(4, 3) == othello::White);
+  REQUIRE(test_board.at(4, 4) == othello::Black);
+}
+
+TEST_CASE("First few moves are possible", "[Board]")
+{
+  othello::Board test_board{};
+  REQUIRE(test_board.place(2, 4, othello::Black) == EXIT_SUCCESS);
+  REQUIRE(test_board.at(2, 4) == othello::Black);
+  REQUIRE(test_board.at(3, 4) == othello::Black);
+  REQUIRE(test_board.at(4, 4) == othello::Black);
+  REQUIRE(test_board.at(3, 3) == othello::Black);
+  REQUIRE(test_board.at(4, 3) == othello::White);
+}
